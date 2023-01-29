8618fbf1eccceb0e2659bab23a88740a99b47bac
mdYeasin-cmd <yeasin200011@gmail.com>
2023-01-29 20:27:55 +0600

    Database connection part added to the project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
+const uri = `mongodb+srv://${process.env.POWER_HACK_DB_USER}:${process.env.POWER_HACK_DB_PASSWORD}@cluster0.0nieed1.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 app.get('/', (req, res) => {
